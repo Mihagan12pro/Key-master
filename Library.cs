48744256e414c4457ccbcbd0fa3e277a93ef6a7b
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-06-28 11:18:21 +0300

    feat: adding dependencies.

--- Library.cs
+++ Library.cs
@@ -1,7 +1,14 @@
-﻿namespace Key_master
+﻿using Key_master.Services;
+using Multicad.Runtime;
+
+namespace Key_master
 {
     internal static class Library
     {
-
+        [CommandMethod("TestMcCAD", CommandFlags.NoCheck | CommandFlags.NoPrefix)]
+        public static void TestMcCADCommands()
+        {
+            DotNetConstantsService.DraftEditor.WriteMessage("Hello  Multicad!");
+        }
     }
 }
