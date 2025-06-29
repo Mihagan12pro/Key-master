84351d25b26207c980a522a804b906dd9de464a0
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-06-29 10:25:36 +0300

    feat: override OnDraw(GeometryBuilder dc)

--- Keys/Key.cs
+++ Keys/Key.cs
@@ -1,10 +1,8 @@
 ﻿using Multicad;
 using Multicad.CustomObjectBase;
-using Multicad.DatabaseServices;
-using Multicad.DatabaseServices.StandardObjects;
 using Multicad.Geometry;
 using Multicad.Runtime;
-
+using System.Configuration;
 
 namespace Key_master.Keys
 {
@@ -14,6 +12,47 @@ namespace Key_master.Keys
         public double Length { get; set; }
 
         public double Width { get; set; }
+
+        private Point3d _point1, _point2;
+
+
+        public override void OnDraw(GeometryBuilder dc)
+        {
+            dc.Clear();
+
+            dc.DrawPolyline
+            (
+                new Point3d[]
+                {
+                    _point1,_point2
+                }    
+            );
+        }
+
+        public override hresult OnMcSerialization(McSerializationInfo info)
+        {
+            info.Add("point1", _point1);
+            info.Add("point2", _point2);
+
+            return hresult.s_Ok;
+        }
+
+        public override hresult OnMcDeserialization(McSerializationInfo info)
+        {
+            if (!info.GetValue("point1",out _point1) || !info.GetValue("point2",out _point2))
+            {
+                return hresult.e_Fail;
+            }
+
+            return hresult.s_Ok;
+        }
+
+
+        public Key()
+        {
+            _point1 = new Point3d(0, 0, 0);
+            _point2 = new Point3d(0, 0, 100);
+        }
     }
 }
 
