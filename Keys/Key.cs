b90f7512c21816f2682656a8d708b71da98732ec
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-07-02 19:55:59 +0300

    fix: no problems with the displacement grip point.

--- Keys/Key.cs
+++ Keys/Key.cs
@@ -11,41 +11,22 @@ using System.Windows.Media;
 namespace Key_master.Keys
 {
     [CustomEntity("1C925FA1-842B-49CD-924F-4ABF9717DB62", "Key", "Key Entity")]
-    internal class Key : McCustomBase, IMcStreamSerializable
+    internal class Key : McCustomBase, IMcSerializable
     {
         public double Length { get; set; }
 
         public double Width { get; set; }
 
-        private Point3d _point,_center;
-        private Vector3d _diagonal;
-
-        
-
-
-        public Point3d Origin
-        {
-            get
-            {
-                return _point;
-            }
-            set
-            {
-                if (!TryModify()) 
-                    return;
-
-                _point = value;
-            }
-        }
+        private Point3d _point1, _point2, _center;
 
 
         public override void OnDraw(GeometryBuilder dc)
         {
             dc.Clear();
 
-            Point3d point2 = _point + _diagonal;
+       
 
-            dc.DrawPolyline(new Point3d[] { _point, new Point3d(_point.X, point2.Y, 0), point2, new Point3d(point2.X, _point.Y, 0), _point });
+            dc.DrawPolyline(new Point3d[] { _point1, new Point3d(_point1.X, _point2.Y, 0), _point2, new Point3d(_point2.X, _point1.Y, 0), _point1 });
         }
 
 
@@ -78,13 +59,33 @@ namespace Key_master.Keys
 
             _center = jig.GetPoint("Куда вставить шпоночный паз: ").Point;
 
-            _point= new Point3d(_center.X + Length * 0.5, _center.Y + Width * 0.5, 0);
-            Point3d point2 = new Point3d(_center.X - Length * 0.5, _center.Y - Width * 0.5, 0);
+            _point1= new Point3d(_center.X + Length * 0.5, _center.Y + Width * 0.5, 0);
+            _point2 = new Point3d(_center.X - Length * 0.5, _center.Y - Width * 0.5, 0);
 
-            _diagonal = new Vector3d(point2.X - _point.X,point2.Y - _point.Y,point2.Z - _point.Z);
+        
 
             DbEntity.Update();
            
+            return hresult.s_Ok;
+        }
+
+
+        public override hresult OnMcSerialization(McSerializationInfo info)
+        {
+            info.Add("point1", _point1);
+            info.Add("point2", _point2);
+            info.Add("center", _center);
+
+            return hresult.s_Ok;
+        }
+
+        public override hresult OnMcDeserialization(McSerializationInfo info)
+        {
+            if (!info.GetValue("point1", out _point1) || !info.GetValue("point2", out _point2) || !info.GetValue("center", out _center))
+            {
+                return hresult.e_Fail;
+            }
+
             return hresult.s_Ok;
         }
 
@@ -94,97 +95,40 @@ namespace Key_master.Keys
             if (!TryModify())
                 return;
             
-            _point = _point.TransformBy(tfm);
+            _point1 = _point1.TransformBy(tfm);
+
+            _point2 = _point1.TransformBy(tfm);
 
             _center = _center.TransformBy(tfm);
-
-            _diagonal = _diagonal.TransformBy(tfm);
         }
 
 
-        public override hresult write(McStream S)
-        {
-            S.PutVersion(1, 0);
 
-            S.PutPoint(_point);
-            S.PutPoint(_center);
-            S.PutVector(_diagonal);
-
-            S.WriteVersionBlockEnd();
-
-            return hresult.s_Ok;
-        }
-
-
-        public override hresult read(McStream S)
-        {
-            short major, minor;
-            if (!S.GetVersion(out major, out minor))
-                return hresult.e_MakeMeProxy;
-
-            if (major == 1)
-            {
-                if (!S.GetPoint(out _point))
-                    return hresult.e_Fail;
-                if (!S.GetVector(out _diagonal))
-                    return hresult.e_Fail;
-                if (!S.GetPoint(out _center))
-                    return hresult.e_Fail;
-            }
-
-            if (!S.ReadVersionBlockEnd())
-                return hresult.e_Fail;
-            return hresult.s_Ok;
-        }
-
+        
 
         public override bool GetGripPoints(GripPointsInfo info)
         {
             info.AppendGrip
                 (
-                    new McSmartGrip<Key>
-                        (
-                            _center, 
-                            (obj, grip, offset) => {
-                                obj.TryModify();
-                                Vector3d vector = _center.GetVectorTo(_center + offset); 
+                   new McSmartGrip<Key>(
+                           _center,
 
+                           (obj, grip, offset) =>
+                           {
+                               if (obj.TryModify())
+                               {
+                                   obj._center += offset;
 
-                                Matrix3d matrixDisplacement = Matrix3d.Displacement(vector);
+                                   Matrix3d matrix3dDisplacement = Matrix3d.Displacement(offset);
 
+                                   obj._point1 = _point1.TransformBy(matrix3dDisplacement);
+                                   obj._point2 = _point2.TransformBy(matrix3dDisplacement);
+                               }
+                           }
+                       )
 
-                                obj.OnTransform(matrixDisplacement);
-
-                                Invalidate();
-                            })
                 );
 
-            info.AppendGrip
-                (
-                    new McSmartGrip<Key>
-                        (
-                            _point,
-                            (obj,grip,offset) => { 
-                                obj.TryModify(); 
-                                obj._diagonal += offset; 
-                            }));
-
-            info.AppendGrip
-                (
-                    new McSmartGrip<Key>
-                        (
-                            _point + _diagonal, 
-                            (obj, grip, offset) => { 
-                                obj.TryModify(); 
-                                obj._diagonal += offset; 
-                            }));
-
-            return true;
-        }
-
-
-        public override bool SupportMcStreamSerialization()
-        {
             return true;
         }
 
