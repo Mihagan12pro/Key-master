99752973a39adcc26bc8fcb34d6756ce55e4fe9c
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-07-12 19:11:32 +0300

    feat: IGeomatryParams and KeyBasic instead of the IKey interface.

---
+++ Keys/IGeometryParams.cs
@@ -0,0 +1,9 @@
+﻿namespace Key_master.Keys
+{
+    internal interface IGeometryParams
+    {
+        double Width { get; set; }
+
+        double Length { get; set; }
+    }
+}
---
+++ Keys/KeyBasic.cs
@@ -0,0 +1,22 @@
+﻿using Multicad;
+using Multicad.CustomObjectBase;
+using Multicad.Geometry;
+using System.Windows.Input;
+
+namespace Key_master.Keys
+{
+    internal abstract class KeyBasic : McCustomBase, IMcSerializable
+    {
+        public string ?KeyType { get; protected set; }
+
+        protected abstract void Scale(KeyBasic obj, Point3d grip, Vector3d offset);
+
+        protected abstract void Displace(Key obj, Point3d grip, Vector3d offset);
+
+        public bool TryModify()
+        {
+            TryModify(0);
+            return true;
+        }
+    }
+}
