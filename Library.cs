a6b3271e8ee07e3bbbb505456024b446dc4fc20f
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-07-13 18:13:52 +0300

    feat: jig input in the CreateCommand().

--- Library.cs
+++ Library.cs
@@ -1,4 +1,8 @@
 ﻿using Key_master.Keys;
+using Multicad;
+using Multicad.DatabaseServices;
+using Multicad.DatabaseServices.StandardObjects;
+using Multicad.Geometry;
 using Multicad.Runtime;
 
 namespace Key_master
@@ -8,9 +12,41 @@ namespace Key_master
         [CommandMethod("KM_create_key", CommandFlags.NoCheck | CommandFlags.NoPrefix)]
         public static void CreateKeyCommand()
         {
-            KeyTypeTwo key = new KeyTypeTwo();
 
-            key.PlaceObject();
+            InputJig jig = new InputJig();
+            jig.SetInputOptions(InputJig.InputReturnMode.Other);
+            jig.ForceInputNumbers = true;
+
+
+            double length;
+            if (!jig.GetRealNumber("Длина шпоночного паза: ", out length))
+                return;
+
+            if (length == 0)
+                length = 50000;
+
+            length = Math.Abs(length);
+
+
+            if (!jig.GetRealNumber("Ширина шпоночного паза: ", out double width))
+                return;
+
+            if (width == 0)
+                width = 50000;
+
+            width = Math.Abs(width);
+
+
+            InputResult result = jig.GetPoint("Выберите, куда вставить шпоночный паз: ");
+
+            if (result.Result == InputResult.ResultCode.Cancel)
+                return;
+
+            Point3d center = result.Point;
+
+
+            KeyTypeTwo typeTwo = new KeyTypeTwo(center, width, length);
+            typeTwo.PlaceObject();
         }
     }
 }
